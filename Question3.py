7dada49f533cee513a5376960b8b0f7c01aa04a7
sathishkarikalan <96059667+sathishkarikalan@users.noreply.github.com>
2022-01-23 19:14:54 +0530

    Add files via upload
    Factorial of a number

---
+++ Question3.py
@@ -0,0 +1,9 @@
+#Factorial of number
+def fact(n):
+  if n==0 :
+    return 1
+  else:
+    return (n*fact(n-1))
+n=int(input("Enter the number:"))
+result=fact(n)
+print("factorial of",n,"is",result)
